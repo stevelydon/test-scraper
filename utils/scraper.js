504c06ef6e5f53dd19bf70553f63d3ae416cd262
steve <156434689+stevelydon@users.noreply.github.com>
2025-08-29 00:02:03 +0200

    BugFix: Change the scraper to now extract 1 by 1
    Before the scraper used cheerio extract method to target
    the data in the html. Found bug where this method misaligns data
    processing and returns incorrect numbers on comment counts and points.
    Due to changing html structure in HackerNews, depending on post
    type. Now processing 1 by 1 with extract and handling errors.

--- utils/scraper.js
+++ utils/scraper.js
@@ -2,37 +2,51 @@ const cheerio = require('cheerio');
 const wordCount = require('./wordCount.js');
 
 async function scrapeHackerNews(){
-	const parsedPosts = [];
+	const posts = [];
 	
+	const parsedHeadlines = [];
+	const parsedSublines = [];
+
 	try{
 		const $ = await cheerio.fromURL("https://news.ycombinator.com/");
-		const posts = $.extract({
-			ranks:['.rank'],
-			titles:['.titleline > a'],
-			points:['.score'],
-			commentCounts:['.subline > a:last-child']
+
+		//change to split extracts, found bug in previous method
+		//sometimes missing points and/or comments. The subline html changes drastically
+		$('.submission').each( function(index,element){
+
+			parsedHeadlines.push($(element).extract({
+				title: '.titleline > a',
+			}));	
+	
 		});
+		$('.submission +').each( function(index,element){
+			parsedSublines.push($(element).extract({
+				points:'.score',
+				commentCount:'.subline > a:last-child',
+			}));
+		});
+
 
 		for(let i = 0;i<30;i++){
 			let parsedPost = {};
 			//taking advantage of parseInt knocking off the rest of string after number
-			let points = parseInt(posts.points[i]);
-			let commentCount = parseInt(posts.commentCounts[i]);
+			let points = parseInt(parsedSublines[i].points);
+			let commentCount = parseInt(parsedSublines[i].commentCount);
 			//sometimes points and comments are missing from HackerNews posts
 			if(isNaN(points)){points = 0}
 			if(isNaN(commentCount)){commentCount = 0}
 			parsedPost["rank"] = i+1;
-			parsedPost["title"] = posts.titles[i];
+			parsedPost["title"] = parsedHeadlines[i].title;
 			parsedPost["points"] = points;
 			parsedPost["commentCount"] = commentCount;
-			parsedPost["titleWordCount"] = wordCount(posts.titles[i]);
-			parsedPosts.push(parsedPost);
+			parsedPost["titleWordCount"] = wordCount(parsedHeadlines[i].title);
+			posts.push(parsedPost);
 		}
 	}catch(err){
 		console.log(err.message);
 	}
 
-	return parsedPosts;
+	return posts;
 }
 
 module.exports = {
